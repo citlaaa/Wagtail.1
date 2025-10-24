ca2becf96ed81cda73e9ecf3782a5c03e008649a
Citlally Suarez <citlally.suarez@comunidad.unam.mx>
2025-10-23 20:22:23 -0600

    Definición del modelo CustomUser con campo 'role' en app users

--- siteuser/settings/base.py
+++ siteuser/settings/base.py
@@ -24,6 +24,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # Application definition
 
 INSTALLED_APPS = [
+    "users",
     "home",
     "search",
     "wagtail.contrib.forms",
@@ -179,3 +180,6 @@ WAGTAILADMIN_BASE_URL = "http://example.com"
 # if untrusted users are allowed to upload files -
 # see https://docs.wagtail.org/en/stable/advanced_topics/deploying.html#user-uploaded-files
 WAGTAILDOCS_EXTENSIONS = ['csv', 'docx', 'key', 'odt', 'pdf', 'pptx', 'rtf', 'txt', 'xlsx', 'zip']
+
+
+AUTH_USER_MODEL = 'users.CustomUser'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Definimos las opciones de ROLES
+ROLE_CHOICES = (
+    ('ADMIN', 'Administrador'),
+    ('EDITOR', 'Editor'),
+    ('CONSULTA', 'Consulta'),
+)
+
+class CustomUser(AbstractUser):
+    # El campo de Rol
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='CONSULTA',
+        verbose_name="Rol de Usuario"
+    )
+
+    # El campo de status (ya incluido en AbstractUser como is_active, pero lo mantenemos visible)
+    # is_active = models.BooleanField(default=True) 
+
+    class Meta:
+        verbose_name = 'Usuario Modular'
+        verbose_name_plural = 'Usuarios Modulares'
+
+    def __str__(self):
+        return self.username
